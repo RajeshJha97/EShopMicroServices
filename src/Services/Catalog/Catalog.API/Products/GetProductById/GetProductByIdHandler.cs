983038405f8a9bb7da99d954bf1febf89cff30c0
Rajesh <rajesh.jha25@yahoo.com>
2026-01-04 22:11:12 +0530

    GetProductsById,GetProductsByCategory

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace Catalog.API.Products.GetProductById;
+
+public record GetProductByIdQuery(Guid Id):IQuery<GetProductByIdResult>;
+public record GetProductByIdResult(Product Product);
+
+internal class GetProductByIdQueryHandler(IDocumentSession session,ILogger<GetProductByIdQueryHandler> logger) : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+{
+    public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetQueryById handler invoked with query {@Query}", query);
+
+        var product = await session.LoadAsync<Product>(query.Id, cancellationToken) 
+            ?? throw new ProductNotFoundException($"Product Not Found With ID: {query.Id}");
+
+        return new GetProductByIdResult(product);
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductsByCategory/GetProductsByCategoryHandler.cs
@@ -0,0 +1,24 @@
+﻿namespace Catalog.API.Products.GetProductsByCategory;
+
+public record GetProductByCategoryQuery(string Category):IQuery<GetProductByCategoryResult>;
+public record GetProductByCategoryResult(IEnumerable<Product>Product);
+
+internal class GetProductsByCategoryQueryHandler(IDocumentSession session,ILogger<GetProductsByCategoryQueryHandler> logger) : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+{
+    public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductByCategory Query Handler Invoked {@Query}", query);
+
+        var products = await session.Query<Product>()
+            .Where(p=>p.Category.Contains(query.Category))
+            .ToListAsync(cancellationToken)??throw new ProductNotFoundException($"Product Not Found With Categroy: {query.Category}");
+
+        if(products.Count==0)
+        {
+            throw new ProductNotFoundException($"Product Not Found With Categroy: {query.Category}");
+        }
+
+        return new GetProductByCategoryResult(products);
+
+    }
+}
